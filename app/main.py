2c53c3c57e18f93a5b302fda00cfa27c963a043f
pagreda <pagreda@outlook.com.pe>
2026-04-15 18:05:43 -0500

    Corregir ruta de archivos estáticos en main.py

--- app/main.py
+++ app/main.py
@@ -482,7 +482,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="../static"), name="static")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 
 # =========================
